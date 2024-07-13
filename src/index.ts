c628f84ddcd413e028b7a2fa3be453f9695f76eb
unknown <vladkashka56@gmail.com>
2024-07-14 01:16:37 +0300

    replace openai with whisper turbo

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,15 @@ import OpenAI from "openai";
 import prism from "prism-media";
 import { Readable, pipeline } from "stream";
 import { default as getUuid, default as uuid } from "uuid-by-string";
+import {
+  AvailableModels,
+  InferenceSession,
+  SessionManager,
+  Segment,
+  DecodingOptionsBuilder,
+  initialize,
+  Task
+} from "whisper-turbo";
 import elaborate_discord from "./actions/elaborate.ts";
 import joinvoice from "./actions/joinvoice.ts";
 import leavevoice from "./actions/leavevoice.ts";
@@ -205,35 +214,82 @@ var openAI = new OpenAI({
   apiKey: settings.OPENAI_API_KEY,
 });
 
-export async function speechToText(buffer: Buffer) {
-  var wavHeader = getWavHeader(buffer.length, 16000);
+// export async function speechToText(buffer: Buffer) {
+//   var wavHeader = getWavHeader(buffer.length, 16000);
 
-  const file = new File([wavHeader, buffer], "audio.wav", {
-    type: "audio/wav",
+//   const file = new File([wavHeader, buffer], "audio.wav", {
+//     type: "audio/wav",
+//   });
+
+//   console.log("Transcribing audio... key", settings.OPENAI_API_KEY);
+//   // This actually returns a string instead of the expected Transcription object 🙃
+//   var result = (await openAI.audio.transcriptions.create(
+//     {
+//       model: "whisper-1",
+//       language: "en",
+//       response_format: "text",
+//       // prompt: settings.OPENAI_WHISPER_PROMPT,
+//       file: file,
+//     },
+//     {
+//       headers: {
+//         Authentication: `Bearer ${settings.OPENAI_API_KEY}`,
+//       },
+//     }
+//   )) as any as string;
+//   result = result.trim();
+//   if (result == null || result.length < 5) {
+//     return null;
+//   }
+//   return result;
+// }
+
+export async function speechToText(buffer: Buffer) {
+
+  const audioData = new Uint8Array(buffer);
+
+  let turboSession;
+
+  let result;
+
+  await initialize();
+
+  const loadResult = await new SessionManager().loadModel(
+    AvailableModels.WHISPER_TINY,
+    () => {
+      console.log("Model loaded successfully");
+    },
+    (p) => {
+      console.log(`Loading: ${p}%`);
+    }
+  );
+
+  if (loadResult.isOk) {
+      turboSession = loadResult.value;
+  } else {
+      console.log("model loading failed")
+  }
+
+
+  let options = new DecodingOptionsBuilder().setTask(Task.Transcribe).build();
+
+
+  await turboSession.transcribe(audioData, true, options, (segment) => {
+      console.log("segment text:", segment.text );
+      result = result.concat(segment.text)
+      if (segment.last) {
+          return
+      }
   });
 
-  console.log("Transcribing audio... key", settings.OPENAI_API_KEY);
-  // This actually returns a string instead of the expected Transcription object 🙃
-  var result = (await openAI.audio.transcriptions.create(
-    {
-      model: "whisper-1",
-      language: "en",
-      response_format: "text",
-      // prompt: settings.OPENAI_WHISPER_PROMPT,
-      file: file,
-    },
-    {
-      headers: {
-        Authentication: `Bearer ${settings.OPENAI_API_KEY}`,
-      },
-    }
-  )) as any as string;
   result = result.trim();
+  console.log(`Speech to text: ${result}`);
   if (result == null || result.length < 5) {
-    return null;
+      return null;
   }
   return result;
 }
+
 export class DiscordClient extends EventEmitter {
   private apiToken: string;
   private client: Client;
--- src/speechtotext.ts
+++ src/speechtotext.ts
@@ -2,36 +2,97 @@ import OpenAI from "openai";
 import settings from "./settings.ts";
 import { getWavHeader } from "./util.ts";
 import { File } from "formdata-node";
+import {
+    AvailableModels,
+    InferenceSession,
+    SessionManager,
+    Segment,
+    DecodingOptionsBuilder,
+    initialize,
+    Task
+  } from "whisper-turbo";
 
 console.log('OpenAI key', settings.OPENAI_API_KEY);
 var openAI = new OpenAI({
     apiKey: settings.OPENAI_API_KEY
 });
 
+// export async function speechToText(buffer: Buffer) {
+//     var wavHeader = getWavHeader(buffer.length, 16000);
+
+//     const file = new File([wavHeader, buffer], 'audio.wav', { type: 'audio/wav' });
+
+//     console.log('Transcribing audio... key', settings.OPENAI_API_KEY);
+//     // This actually returns a string instead of the expected Transcription object 🙃
+//     var result = await openAI.audio.transcriptions.create({
+//         model: 'whisper-1',
+//         language: 'en',
+//         response_format: 'text',
+//         // prompt: settings.OPENAI_WHISPER_PROMPT,
+//         file: file,
+//     }, 
+//     {
+//         headers: {
+//             "Authentication": `Bearer ${settings.OPENAI_API_KEY}`,
+//         }
+//     }) as any as string;
+
+//     result = result.trim();
+//     console.log(`Speech to text: ${result}`);
+//     if (result == null || result.length < 5) {
+//         return null;
+//     }
+//     return result;
+// }
+
+
 export async function speechToText(buffer: Buffer) {
-    var wavHeader = getWavHeader(buffer.length, 16000);
 
-    const file = new File([wavHeader, buffer], 'audio.wav', { type: 'audio/wav' });
+    const audioData = new Uint8Array(buffer);
 
-    console.log('Transcribing audio... key', settings.OPENAI_API_KEY);
-    // This actually returns a string instead of the expected Transcription object 🙃
-    var result = await openAI.audio.transcriptions.create({
-        model: 'whisper-1',
-        language: 'en',
-        response_format: 'text',
-        // prompt: settings.OPENAI_WHISPER_PROMPT,
-        file: file,
-    }, 
-    {
-        headers: {
-            "Authentication": `Bearer ${settings.OPENAI_API_KEY}`,
+    let turboSession;
+
+    let result;
+
+    await initialize();
+
+    const loadResult = await new SessionManager().loadModel(
+      AvailableModels.WHISPER_TINY,
+      () => {
+        console.log("Model loaded successfully");
+      },
+      (p) => {
+        console.log(`Loading: ${p}%`);
+      }
+    );
+  
+    if (loadResult.isOk) {
+        turboSession = loadResult.value;
+    } else {
+        console.log("model loading failed")
+    }
+
+
+    let options = new DecodingOptionsBuilder().setTask(Task.Transcribe).build();
+  
+  
+    await turboSession.transcribe(audioData, true, options, (segment) => {
+        console.log("segment text:", segment.text );
+        result = result.concat(segment.text)
+        if (segment.last) {
+            return
         }
-    }) as any as string;
+    });
 
     result = result.trim();
     console.log(`Speech to text: ${result}`);
     if (result == null || result.length < 5) {
         return null;
     }
+    
     return result;
+
 }
+
+
+
